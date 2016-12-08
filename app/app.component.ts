e858ae4f423cd2ec5c5f58cd53d84b69940a4467
Tomas Larson <tomas.g.larson@gmail.com>
2016-12-08 10:24:06 -0600

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  styleUrls: ['./app/app.css'],
+  templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+  message = 'This is the sample message.';
+}
---
+++ dist/app.routing.js
@@ -0,0 +1,11 @@
+"use strict";
+var router_1 = require("@angular/router");
+var home_component_1 = require("./home/home.component");
+var appRoutes = [
+    {
+        path: '',
+        component: home_component_1.HomeComponent
+    }
+];
+exports.appRouting = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
